2bcb81a106ddafd9793b6c051d24ccea92c189ad
Farid Mansour Thabet Alabsi <alabsifarid@gmail.com>
2018-06-25 20:08:29 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,47 +1,7 @@
-
-function getIt() {
-  $('p').on('click', function() {
-    alert('Hey!');
-  });
-}
-
-function frameIt() {
-  $('img').on('load', function() {
-    $('img').addClass('tasty');
-  });
-}
-
-function pressIt() {
-  $('input').on('keydown', function(key) {
-    if(key.which == 71){
-        alert('g was pressed');
-    }
-  });
-}
-
-function submitIt() {
-  $('form').on('submit', function() {
-    alert('Your form is going to be submitted now.');
-  });
-}
+//define functions here
 
 $(document).ready(function(){
 
 // call functions here
- 
-});//define functions here
-
-$(document).ready(function(){
-
-// call functions here
- getIt();
-
-  frameIt();
-
-  pressIt();
-
-  submitIt();
-
-
 
 });
